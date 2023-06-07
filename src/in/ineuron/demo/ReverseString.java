7c3934f1dfd3b297164e74481fa6b136ed83777b
samadrita-Chatterjee <samadritachatterjee25@gmail.com>
2023-06-07 23:28:05 +0530

    first commit

---
+++ src/in/ineuron/demo/ReverseString.java
@@ -0,0 +1,14 @@
+package in.ineuron.demo;
+
+public String ReverseString(String s, int k) {
+    char[] ca = s.toCharArray();
+    for (int left = 0; left < ca.length; left += 2 * k) {
+        for (int i = left, j = Math.min(left + k - 1, ca.length - 1); i < j; i++, j--) {
+            char tmp = ca[i];
+            ca[i] = ca[j];
+            ca[j] = tmp;
+        }
+    }
+    return new String(ca);
+}
+}
